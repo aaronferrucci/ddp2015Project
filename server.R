d9b01de3229fb1b78c89f089a12863ca229201da
Aaron Ferrucci <aaronf@antfarm.org>
2015-11-18 20:36:04 -0800

    Enable jitter, smooter; get rid of model tab.

--- server.R
+++ server.R
@@ -104,86 +104,14 @@ function(input, output) {
     }
     p <- p + ggtitle(title_string(input$y, input$x))
 
+    if (input$jitter)
+      p <- p + geom_jitter()
+
+    if (input$smooth)
+      p <- p + stat_smooth(method = "lm", formula = y ~ x)
     print(p)
 
   }, height=600)
 
-  select_data <- reactive({
-    dataset[dataset$overall <= as.numeric(input$max_rank2),]
-  })
-
-  formula_string <- reactive({
-    fs <- paste0(input$outcome, " ~ ", input$cov1)
-    if (input$cov2 != "None")
-      fs = paste0(fs, " + ", input$cov2)
-    fs
-  })
-
-  model <- reactive({
-    fmla <- as.formula(formula_string())
-    lm(fmla, data=select_data())
-  })
-
-  output$plot2 <- renderPlot({
-    pred <- data.frame(predict(model(), interval = ("prediction")))
-    allDat <- cbind(select_data(), pred)
-    p <- ggplot(allDat, aes_string(x=input$cov1, y=input$outcome))
-    if (input$cov2 == "None")
-      p <- p + geom_point()
-    else
-      p <- p + geom_point(aes_string(col=input$cov2))
-    p <- p + geom_line(aes_string(x=input$cov1, y="fit"))
-    # p <- p + geom_ribbon(aes(ymin = lwr, ymax = upr), alpha = 0.2)
-
-    # Format an "age" axis.
-    if (input$cov1 == "age") {
-      p <- p + scale_x_continuous(breaks = seq(0, 100, 10))
-    } 
-    if (input$outcome == "age") {
-      p <- p + scale_y_continuous(breaks = seq(0, 100, 10))
-    }
-
-    # Format the "elapsed time" axis.
-    if (input$cov1 == "elapsed") {
-      p <- p + scale_x_continuous(
-        breaks = elapsed_ticks,
-        labels = timestr(elapsed_ticks),
-        name = "elapsed time (h:mm:ss)"
-      )
-    }
-    if (input$outcome == "elapsed") {
-      p <- p + scale_y_continuous(
-        breaks = elapsed_ticks,
-        labels = timestr(elapsed_ticks),
-        name = "elapsed time (h:mm:ss)"
-      )
-    }
-
-    # Format the "start time" axis"
-    if (input$cov1 == "start") {
-      p <- p + scale_x_continuous(
-        breaks = start_ticks,
-        labels = timestr(start_ticks),
-        name = "start time (AM)"
-      ) + expand_limits(x = hours_to_ms(8.5))
-    }
-    if (input$outcome == "start") {
-      p <- p + scale_y_continuous(
-        breaks = start_ticks,
-        labels = timestr(start_ticks),
-        name = "start time (AM)"
-      ) + expand_limits(y = hours_to_ms(8.5))
-    }
-    p <- p + ggtitle(sprintf("%s, model: %s",
-      title_string(input$outcome, input$cov1),
-      formula_string()
-    ))
-
-    print(p)
-  }, height=600)
-
-  output$modelSummary <- renderPrint({
-    paste0("R Squared: ", summary(model())$r.squared)
-  })
 }
 
--- ui.R
+++ ui.R
@@ -32,18 +32,11 @@ can look up runners' results by bib number, name, city and state. This
 is ok, but it's not very handy for anyone wanting to do real data analysis.",
 br(),
 br(),
-"This web application facilitates analysis of the 2015 race results. On the 
-'Data Exploration' tab, you can explore the data in a plot, with free choice of
-variables for X and Y axes, and color. You can also limit the displayed data 
-those under a specific overall rank. On the 'Modeling' tab, you can choose 
-variables to be applied as predictor and outcome. For example, how well can we
-predict elapsed time by using age and sex as covariates? The race data are
-displayed in a plot, along with the predicted fit values."
+"This web application facilitates exploration of the 2015 race results. On 
+the 'Data Exploration' tab, you can plot the data in various ways."
           )
         )
       ),
-# To do: provide summary info, maybe for the y axis variable? I have some
-# unused space.
       tabPanel("Data Exploration", 
         fluidRow(
           br(),
@@ -60,36 +53,11 @@ displayed in a plot, along with the predicted fit values."
                 max(dataset$overall)
               ),
               max(dataset$overall)
-            )
+            ),
+            checkboxInput('smooth', 'Enable Smoother', FALSE),
+            checkboxInput('jitter', 'Enable Jitter', FALSE)
           ),
           column(8, plotOutput('plot'))
-        )
-      ),
-# To do: choose y ~ a + b + c (at least one, at most three covariates)
-# Provide a graph of y ~ a, with error bands or other statistics
-# Provide R^2, and an explanation of its meaning.
-# Some explanatory text, for example if I'm modeling bib# ~ start, what
-# does that mean, if anything?
-      tabPanel("Modeling",
-        fluidRow(
-          br(),
-          column(3, offset=1,
-            selectInput('outcome', 'Outcome', useful_names, "elapsed"),
-            selectInput('cov1', 'Covariate 1', useful_names, "age"),
-            selectInput('cov2', 'Covariate 2', c("None", useful_names), "sex"),
-            selectInput(
-              'max_rank2',
-              'Limit to rank',
-              c(
-                100,
-                seq(1000, 13000, 1000),
-                max(dataset$overall)
-              ),
-              1000 
-            ),
-            verbatimTextOutput("modelSummary")
-          ),
-          column(8, plotOutput('plot2'))
         )
       )
     )
