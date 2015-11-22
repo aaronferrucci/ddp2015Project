cf01c2dcd7f4fb701f42ff8194cfe18620e0e517
Aaron Ferrucci <aaronf@antfarm.org>
2015-11-21 19:17:57 -0800

    Change to a two-panel view - README and Data Exploration.

--- ui.R
+++ ui.R
@@ -12,9 +12,10 @@ useful_names = c(
   "start time" = "start"
 )
 
-shinyUI(
-  pageWithSidebar(
-    headerPanel("Wharf to Wharf 2015 Explorer"),
+fluidPage(
+  titlePanel("Wharf to Wharf 2015 Explorer"),
+  # Sidebar with a slider input for the number of bins
+  sidebarLayout(
     sidebarPanel(
       selectInput('y', 'Y', useful_names, "elapsed"),
       selectInput('x', 'X', useful_names, "age"),
@@ -30,23 +31,52 @@ shinyUI(
         max(dataset$overall)
       ),
       checkboxInput('smooth', 'Enable Smoother', FALSE),
-      checkboxInput('jitter', 'Enable Jitter', FALSE),
-      helpText(
-        "This app lets you explore the results of the 2015 Wharf to Wharf 
-        race in Santa Cruz, CA. Data from the race is plotted according to 
-        chosen X and Y variables, with coloring based on a 3rd variable. 
-        The data size can be limited by overall runner rank.",
-        br(),
-        br(),
-        "If the smoother is enabled, trends are more visible. For example, 
-        try elapsed time vs. age, colored by sex, with smoothing, and limit 
-        the data to the top 100 runners.",
-        br(),
-        br(),
-        "Jitter is useful when one of the axes is a factor variable, that is,
-        sex or country."
-      )
+      checkboxInput('jitter', 'Enable Jitter', FALSE)
     ),
-    mainPanel(plotOutput("plot"))
+
+    # Show a plot of the generated distribution
+    mainPanel(
+      tabsetPanel(
+        tabPanel(
+          "README",
+          fluidRow(
+            br(),
+            column(8, offset=2,
+              "The Wharf to Wharf race happens every July in Santa Cruz. The mostly flat,
+              6-mile course hugs the coastline between Santa Cruz and Capitola. The 
+              race has grown in popularity since its beginning in 1973, with more than 13,000
+              registered participants in 2015.",
+              br(),
+              br(),
+              "Race results are available online. You
+              can look up runners' results by bib number, name, city and state. This
+              is ok, but it's not very handy for anyone wanting to do real data analysis.",
+              br(),
+              br(),
+              "This application facilitates exploration of the 2015 Wharf
+              to Wharf Race results. On the 'Data Exploration' tab, the data 
+              is plotted in various ways, under control of the widgets on the 
+              left. You can choose variables for the X and Y axes, as well 
+              as color.",
+              br(),
+              br(),
+              "The 'Limit to Rank' control restricts the displayed data to
+              those runners who achieved a given overall ranking or better.
+              ",
+              br(),
+              br(),
+              "The controls also have a couple of checkboxes: 'Enable Smoother'
+              shows trend lines, which can clarify the direction the data is
+              going; 'Enable Jitter' is helpful when an axis shows a factor
+              variable (sex, country)."
+            )
+          )
+        ),
+        tabPanel("Data Exploration", 
+          br(),
+          column(12, plotOutput('plot'))
+        )
+      )
+    )
   )
 )
